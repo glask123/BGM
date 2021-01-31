696e8d240e88947ae4b33086078559e37317ed6f
glask123 <gschulzekalt@gmail.com>
2021-01-30 23:23:56 -0800

    fix

--- backend/main.js
+++ backend/main.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 const Joi = require('joi');
-const mongoose = require('mongoose');
 require('dotenv/config');
 const userRoute = require('./routes/users');
 
 app.use('/users', userRoute);
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', {root: __dirname })
+    res.send("Hello");
 });
 
+/*
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("Connected to MongoDB");
 });
+*/
 
-app.listen(5500);
+app.listen(3000);
